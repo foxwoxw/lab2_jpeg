523b007e5c8d5fddcd5b31c7c8b5fcca4c647683
foxwoxw <133914722+foxwoxw@users.noreply.github.com>
2024-05-28 19:21:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import jpeg
 import generate_test_data as gtd
+from PIL import Image
 
 
 #gtd.one_color(70, 130, 180, 700, 1000)
 #gtd.random_color(700, 1000)
+#gtd.basic_color(128)
+#gtd.blocks(768, 768, 64)
+
+
 
 def main():
-    print('Выбор изображения: 1 - цветное изображение, 2 - однотонное, 3 - случайный цвет.')
-    print('type = ')
+    print('Выбор изображения: 1 - цветное изображение, 2 - однотонное, 3 - случайный цвет,\n\t\t   4 - базовые цвета, 5 - блоки из нескольких цветов.')
+    print('Изображение: ', end = '')
     img_type = int(input())
     if img_type == 1:
         file = 'color.png'
@@ -15,12 +20,16 @@ def main():
         file = 'one_color.png'
     elif img_type == 3:
         file = 'random_color.png'
+    elif img_type == 4:
+        file = 'basic_color.png'
+    elif img_type == 5:
+        file = 'blocks.png'
     else:
         print('error')
         return
     print('Качество: Q = ', end = '')
     q = int(input())
-    if q < 0 or q > 100:
+    if q < 1 or q > 100:
         print('error')
         return
     jpeg.encode_jpeg('png\\' + file, 2, 2, q)
@@ -28,8 +37,15 @@ def main():
     print('Коэффициент сжатия: ', end = '')
     print(gtd.compression_ratio(file))
     print('Выполняется декодирование изображения..')
-    jpeg.decode_jpeg('txt\\result.txt')
+    rgb = jpeg.decode_jpeg('txt\\result.txt')
+    print("Сохранить? ('y'/'n'): ", end = '')
+    a = input()
+    if a == 'y':
+        print("Введите имя файла: ", end = '')
+        a = input()
+        Image.fromarray((rgb),mode="RGB").save('results\\' + a)
 
 
 
 main()
+
